4b63f971b525a56df68e9a5718664350e2f47510
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2022-06-21 22:15:22 +0800

    osmApi初步完成（单线程运作）

--- geo.py
+++ geo.py
@@ -2,14 +2,11 @@ import json
 import logging
 import time
 
-import pandas as pd
 import requests
 
-from translate import translate
-
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-spToEngDict = json.load(open('assets/assets.json', 'r', encoding='utf-8'))
 iso3166MapDict = json.load(open('assets/iso3166-1.json', 'r', encoding='utf-8'))
+session = requests.session()
 
 
 def getRawData(rawData: dict) -> list:
@@ -36,62 +33,53 @@ def getRawData(rawData: dict) -> list:
     return geoRawDataList
 
 
-def search(country: str, prov: str, geocodingCsvPath: str):
+def search(country: str, prov: str):
     """
     根据国家和省份查询经纬度 by English
     :param country: str, 国家
     :param prov: str, 省份
-    :param geocodingCsvPath: str, 经纬度文件路径
     :return: tuple(lat:float, lng:float)
     """
-    df = pd.read_csv(geocodingCsvPath, delimiter=",")
-    tmp = None
-    for index, row in df.iterrows():
-        if (str(row['Country']) in country) or (country in str(row['Country'])):
-            tmp = row['lat'], row['lng']
-            if prov == '' and (country == 'China' or country == 'CN'):
-                return None
-            if (str(row['Province']) in prov) or (prov in str(row['Province'])):
-                return row['lat'], row['lng']
-    for index, row in df.iterrows():
-        if (str(row['Province']) in prov) or (prov in str(row['Province'])):
-            return row['lat'], row['lng']
-    logging.info('{} {} not match,return {}'.format(country, prov, tmp))
-    return tmp
+    # tmp = None
+    # for index, row in df.iterrows():
+    #     if (str(row['Country']) in country) or (country in str(row['Country'])):
+    #         tmp = row['lat'], row['lng']
+    #         if prov == '' and (country == 'China' or country == 'CN'):
+    #             return None
+    #         if (str(row['Province']) in prov) or (prov in str(row['Province'])):
+    #             return row['lat'], row['lng']
+    # for index, row in df.iterrows():
+    #     if (str(row['Province']) in prov) or (prov in str(row['Province'])):
+    #         return row['lat'], row['lng']
+    # logging.info('{} {} not match,return {}'.format(country, prov, tmp))
+    # return tmp
+    addr = prov + ',' + country
+    try:
+        r = session.get(f'https://nominatim.openstreetmap.org/search/{addr}?limit=1&format=json')
+        r = r.json()[0]
+    except IndexError:
+        logging.info('{} {} not found by osmApi'.format(country, prov))
+        return None
+    return r['lat'], r['lon']
 
 
-def toEnglish(session: requests.session, text: str) -> str:
-    """
-    转换为英文
-    :param session:
-    :param text: str, 要转换的文本
-    :param target: str, 目标语言
-    :return: str, 英文
-    """
-    if text in spToEngDict:
-        return spToEngDict[text]
-    else:
-        return translate(session, text, "en", "auto")
-
-
-def geocodingSingle(session: requests.session, addrList: list) -> list:
+def geocodingSingle(addrList: list) -> list:
     """
     单个地址转经纬度
     :param addrList: list, 地址信息，格式为[国家, 省份]
-    :param session:
     :return: list[lat:float, lng:float], 经纬度
     """
     if len(addrList[0]) == 2:
         if addrList[0] in iso3166MapDict:
             country = iso3166MapDict[addrList[0]]
         else:
-            country = toEnglish(session, addrList[0])
+            country = addrList[0]
     else:
-        country = toEnglish(session, addrList[0])
+        country = addrList[0]
     logging.debug('country: {}'.format(country))
-    prov = toEnglish(session, addrList[1])
+    prov = addrList[1]
     logging.debug('prov: {}'.format(prov))
-    coordinateTuple = search(country, prov, 'assets/geocoding2.csv')
+    coordinateTuple = search(country, prov)
     if coordinateTuple:
         return [coordinateTuple[0], coordinateTuple[1]]
     else:
@@ -100,16 +88,15 @@ def geocodingSingle(session: requests.session, addrList: list) -> list:
         return []
 
 
-def geocoding(geoRawDataList: list, session: requests.session) -> list:
+def geocoding(geoRawDataList: list) -> list:
     """
     多个地址转经纬度
     :param geoRawDataList: list, 地址集:[['Country0']['Prov0'],['Country1']['Prov1'],...]
-    :param session:
     :return: list, 经纬度:[[lat0:float, lng0:float],[lat1:float, lng1:float],...]
     """
     coordinatesList = []
     for i in geoRawDataList:
-        coordinateList = geocodingSingle(session, i)
+        coordinateList = geocodingSingle(i)
         if len(coordinateList):
             coordinatesList.append(coordinateList)
     return coordinatesList
@@ -124,7 +111,7 @@ def geoInterface(rawData: dict) -> list:
     session = requests.session()
     geoRawDataList = getRawData(rawData)
     logging.debug('geoRawDataList: {}'.format(geoRawDataList))
-    coordinatesList = geocoding(geoRawDataList, session)
+    coordinatesList = geocoding(geoRawDataList)
     if not coordinatesList:
         logging.warning('没有搜索到任何数据\nrawData:\n{}'.format(rawData))
     return coordinatesList
