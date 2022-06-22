19981306789d6579a491c000e0a9a6b08c203163
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2022-06-22 17:49:36 +0800

    准备合并

--- geo.py
+++ geo.py
@@ -1,10 +1,7 @@
 import json
 import logging
 
-import pandas as pd
 import requests
-
-from translate import translate
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 spToEngDict = json.load(open('assets/assets.json', 'r', encoding='utf-8'))
@@ -36,48 +33,10 @@ def getRawData(rawData: dict) -> list:
     return geoRawDataList
 
 
-# TODO 改为多线程
-def toEnglish(text: str) -> str:
-    """
-    转换为英文
-    :param text: str, 要转换的文本
-    :return: str, 英文
-    """
-    if text in spToEngDict:
-        return spToEngDict[text]
-    else:
-        return translate(session, text, "en", "auto")
-
-
-def search(country: str, prov: str, geocodingCsvPath: str = 'assets/geocoding2.csv'):
-    """
-    根据国家和省份查询经纬度 by English
-    :param country: str, 国家
-    :param prov: str, 省份
-    :param geocodingCsvPath: str, 经纬度文件路径
-    :return: tuple(lat:float, lng:float, msg:str)
-    """
-    country=toEnglish(country)
-    prov=toEnglish(prov)
-    df = pd.read_csv(geocodingCsvPath, delimiter=",")
-    tmp = None
-    for index, row in df.iterrows():
-        if (str(row['Country']) in country) or (country in str(row['Country'])):
-            tmp = row['lat'], row['lng'], prov + ',' + country
-            if prov == '' and (country == 'China' or country == 'CN'):
-                return None
-            if (str(row['Province']) in prov) or (prov in str(row['Province'])):
-                return row['lat'], row['lng'], prov + ',' + country
-    for index, row in df.iterrows():
-        if (str(row['Province']) in prov) or (prov in str(row['Province'])):
-            return row['lat'], row['lng'], prov + ',' + country
-    logging.info('{} {} not match,return {}'.format(country, prov, tmp))
-    return tmp
-
-
-def geocodingSingle(addrList: list):
+def geocodingSingle(addrList: list, localQuery: bool):
     """
     单个地址转经纬度
+    :param localQuery: bool, 是否使用本地数据
     :param addrList: list, 地址信息，格式为[国家, 省份]
     :return: list[lat:float, lng:str, msg:float], 经纬度
     """
@@ -91,6 +50,10 @@ def geocodingSingle(addrList: list):
     logging.debug('country: {}'.format(country))
     prov = addrList[1]
     logging.debug('prov: {}'.format(prov))
+    if localQuery:
+        from localQuery import search
+    else:
+        from externalQuery import search
     coordinateTuple = search(country, prov)
     if len(coordinateTuple)==3:
         logging.debug(f"coordinateTuple: {[coordinateTuple[0], coordinateTuple[1], coordinateTuple[2]]}")
@@ -101,28 +64,19 @@ def geocodingSingle(addrList: list):
         return None
 
 
-def geocoding(geoRawDataList: list) -> list:
-    """
-    多个地址转经纬度
-    :param geoRawDataList: list, 地址集:[['Country0','Prov0'],['Country1','Prov1'],...]
-    :return: list, 经纬度:[[lat0:float, lng0:float, msg0:str],[lat1:float, lng1:float, msg1:str],...]
-    """
-    coordinatesList = []
-    for i in geoRawDataList:
-        coordinateList = geocodingSingle(i)
-        if len(coordinateList):
-            coordinatesList.append(coordinateList)
-    return coordinatesList
-
-
-def geoInterface(rawData: dict) -> list:
+def geoInterface(rawData: dict, localQuery: bool) -> list:
     """
     地址转经纬度接口
+    :param localQuery: bool, 是否使用本地数据
     :param rawData: dict, 原始数据
     :return: list, 经纬度:[[lat0:float, lng0:float, msg0:str],[lat1:float, lng1:float, msg1:str],...]
     """
     geoRawDataList = getRawData(rawData)
     logging.debug('geoRawDataList: {}'.format(geoRawDataList))
+    if localQuery:
+        from localQuery import geocoding
+    else:
+        from externalQuery import geocoding
     coordinatesList = geocoding(geoRawDataList)
     if not coordinatesList:
         logging.warning('没有搜索到任何数据\nrawData:\n{}'.format(rawData))
---
+++ localQuery.py
@@ -0,0 +1,66 @@
+import json
+import logging
+
+import pandas as pd
+import requests
+
+import translate
+import geo
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+spToEngDict = json.load(open('assets/assets.json', 'r', encoding='utf-8'))
+iso3166MapDict = json.load(open('assets/iso3166-1.json', 'r', encoding='utf-8'))
+session = requests.session()
+
+
+# TODO 改为多线程
+def toEnglish(text: str) -> str:
+    """
+    转换为英文
+    :param text: str, 要转换的文本
+    :return: str, 英文
+    """
+    if text in spToEngDict:
+        return spToEngDict[text]
+    else:
+        return translate.translate(session, text, "en", "auto")
+
+
+def search(country: str, prov: str, geocodingCsvPath: str = 'assets/geocoding2.csv'):
+    """
+    根据国家和省份查询经纬度 by English
+    :param country: str, 国家
+    :param prov: str, 省份
+    :param geocodingCsvPath: str, 经纬度文件路径
+    :return: tuple(lat:float, lng:float, msg:str)
+    """
+    country = toEnglish(country)
+    prov = toEnglish(prov)
+    df = pd.read_csv(geocodingCsvPath, delimiter=",")
+    tmp = None
+    for index, row in df.iterrows():
+        if (str(row['Country']) in country) or (country in str(row['Country'])):
+            tmp = row['lat'], row['lng'], prov + ',' + country
+            if prov == '' and (country == 'China' or country == 'CN'):
+                return None
+            if (str(row['Province']) in prov) or (prov in str(row['Province'])):
+                return row['lat'], row['lng'], prov + ',' + country
+    for index, row in df.iterrows():
+        if (str(row['Province']) in prov) or (prov in str(row['Province'])):
+            return row['lat'], row['lng'], prov + ',' + country
+    logging.info('{} {} not match,return {}'.format(country, prov, tmp))
+    return tmp
+
+
+def geocoding(geoRawDataList: list) -> list:
+    """
+    多个地址转经纬度
+    :param geoRawDataList: list, 地址集:[['Country0','Prov0'],['Country1','Prov1'],...]
+    :return: list, 经纬度:[[lat0:float, lng0:float, msg0:str],[lat1:float, lng1:float, msg1:str],...]
+    """
+    coordinatesList = []
+    for i in geoRawDataList:
+        coordinateList = geo.geocodingSingle(i, True)
+        if len(coordinateList):
+            coordinatesList.append(coordinateList)
+    return coordinatesList
