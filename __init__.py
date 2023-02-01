74e1a218a2f40ad2d24122607e7321d6c2975df7
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2023-02-02 00:49:08 +0800

    增加一些信息的显示，修复偶尔发生的地图中心点不正确的BUG

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ import json
 import logging
 import os
 import re
+import random
 from multiprocessing.dummy import Pool as ThreadPool
 from typing import Union
 from urllib import parse
@@ -14,5 +15,5 @@ import html
 
 if __name__ == '__main__':
     _ = str(json) + str(logging) + str(ThreadPool()) + str(datetime) + str(os) + str(html) + str(re) + str(Union) + \
-        str(parse) + str(requests) + str(HTTPAdapter)
+        str(parse) + str(requests) + str(HTTPAdapter) + str(random)
     print(_)
--- main.py
+++ main.py
@@ -8,23 +8,41 @@ localQuery = True
 
 
 def draw(locationsRawList: list, output_path: str, file_name: str) -> None:
+    """
+    绘制traceMap
+    :param locationsRawList: list, 需要绘制轨迹的经纬度信息，格式为[[lat0, lon0, msg0], [lat1, lon1, msg1], ...] (纬度,经度,城市名,信息)
+    :param output_path: str, 轨迹图保存路径
+    :param file_name: str, 轨迹图保存文件名
+    """
     # 计算中心
     location_center_lat = (locationsRawList[0][0] + locationsRawList[-1][0]) / 2
-    location_center_lng = (locationsRawList[0][1] + locationsRawList[-1][1]) / 2
-    content = 'map.centerAndZoom(new BMapGL.Point(%s, %s), 4)\n' % (location_center_lng, location_center_lat)
+    if abs(locationsRawList[0][1] - locationsRawList[-1][1]) > 180:
+        location_center_lng = (locationsRawList[0][1] + locationsRawList[-1][1] + 360) / 2
+    else:
+        location_center_lng = (locationsRawList[0][1] + locationsRawList[-1][1]) / 2
+    content = 'map.centerAndZoom(new BMapGL.Point({}, {}), 4)\n'.format(location_center_lng, location_center_lat)
     for i in locationsRawList:
-        # content += 'AddLabel(map, "%s", %s, %s)\n' % (i[2], i[0], i[1])
-        content += 'AddPathPoint(path, %s, %s)\n' % (i[0], i[1])
-        content += 'AddPoint(map, "%s", "%s", %s, %s)\n' % (i[2], i[3], i[0], i[1])
+        lat = i[0] + random.uniform(-0.1, 0.1)
+        lng = i[1] + random.uniform(-0.1, 0.1)
+        text = i[5] + ' ' \
+               + (('AS' + i[4]) if i[4] != '' else '') + ' ' \
+               + 'TTL:' + i[7] + ' ' \
+               + i[3] + ' ' \
+               + 'RTT:' + i[8] + 'ms'  # + i[6]
+        # text = i[3]
+        # content += 'AddLabel(map, "{}", {}, {})\n'.format(i[2], i[0], i[1])
+        content += 'AddPathPoint(path, {}, {})\n'.format(lat, lng)
+        content += 'AddPoint(map, "{}", "{}", {}, {})\n'.format(i[2], text, lat, lng)
     template = ''
     with open('/var/www/traceMap/template.html', 'r') as f:
         template = f.read()
     new_content = template.replace("%_REPLACE_CONTENT_%", content)
     # print(new_content)
-    f = open(os.path.join(output_path, file_name),'w')
+    f = open(os.path.join(output_path, file_name), 'w')
     f.write(new_content)
     f.close()
-        
+
+
 # def draw(locationsRawList: list, output_path: str, file_name: str) -> None:
 #     """
 #     绘制traceMap
@@ -76,7 +94,7 @@ def process(rawData: Union[dict, str], filename=str(int(datetime.datetime.now().
         for j in k:
             if j['Success']:
                 if not 'lat' in j['Geo']:
-                    return "不受支持的版本，请更新至 NextTrace，因时间精力等问题，我们已经暂时放弃对 Enhanced 版本的继续支持"
+                    return "不受支持的版本，请更新至最新版本NextTrace。"
                 if j['Geo']['lat'] == 0 and j['Geo']['lng'] == 0:
                     continue
                 if j['Geo']['prov'] == "" and j['Geo']['country'] in ['中国', '美国', '俄罗斯']:
@@ -88,7 +106,18 @@ def process(rawData: Union[dict, str], filename=str(int(datetime.datetime.now().
                     tmpCity = j['Geo']['prov']
                 if j['Geo']['city'] != '':
                     tmpCity = j['Geo']['city']
-                coordinatesList.append([j['Geo']['lat'], j['Geo']['lng'], tmpCity, j['Geo']['owner']])
+                coordinatesList.append(
+                    [
+                        j['Geo']['lat'], j['Geo']['lng'],
+                        tmpCity,
+                        j['Geo']['owner'],
+                        j['Geo']['asnumber'] if 'asnumber' in j['Geo'] else '',
+                        j['Address']['IP'] if 'IP' in j['Address'] else '',
+                        j['whois'] if 'whois' in j else '',
+                        f'{j["TTL"]}' if 'TTL' in j else '',
+                        f'{(j["RTT"] / 1_000_000):.2f}' if 'RTT' in j else ''  # unit: ms
+                    ]
+                )
                 break
     draw(coordinatesList, './html', filename)
     return urlPrefix + filename
